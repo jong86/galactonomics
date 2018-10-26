efc305ceadb5d0a642db257867678ea3dc61899f
Jon Gaspar <jongaspar@gmail.com>
2018-10-25 19:19:03 -0700

    restricts to only one spaceship per account

--- contracts/GalacticTransitAuthority.sol
+++ contracts/GalacticTransitAuthority.sol
@@ -3,13 +3,23 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract GalacticTransitAuthority is ERC721 {
+  struct Spaceship {
+    
+  }
+
   uint numSpaceships;
+
+  mapping(uint => Spaceship) public spaceships;
 
   constructor() public ERC721() {}
 
   function buySpaceship() external payable {
     numSpaceships++;
     _mint(msg.sender, numSpaceships);
+  }
+
+  function travelToPlanet(uint8 _planetId) external {
+
   }
 
   function() public {}
