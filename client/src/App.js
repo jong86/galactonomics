1e3608bfefc1e588ec4e40dd2b79b94d094096f5
Jon Gaspar <jongaspar@gmail.com>
2018-10-30 16:46:10 -0700

    added react-jss

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import gtaContract from "./contracts/GalacticTransitAuthority.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = {
+    infoFromGTA: 0,
+    web3: null,
+    accounts: null,
+    contract: null,
+  };
 
   componentDidMount = async () => {
     try {
@@ -17,7 +22,7 @@ class App extends Component {
       const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const Contract = truffleContract(SimpleStorageContract);
+      const Contract = truffleContract(gtaContract);
       Contract.setProvider(web3.currentProvider);
       const instance = await Contract.deployed();
 
@@ -36,14 +41,11 @@ class App extends Component {
   runExample = async () => {
     const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    await contract.set(5, { from: accounts[0] });
-
     // Get the value from the contract to prove it worked.
-    const response = await contract.get();
+    const response = await contract.getInfo();
 
     // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
+    this.setState({ infoFromGTA: JSON.stringify(response) });
   };
 
   render() {
@@ -62,7 +64,7 @@ class App extends Component {
         <p>
           Try changing the value stored on <strong>line 37</strong> of App.js.
         </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>The stored value is: {this.state.infoFromGTA}</div>
       </div>
     );
   }
