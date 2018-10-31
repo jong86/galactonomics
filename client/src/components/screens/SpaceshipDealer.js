5d8e2048781b6d3f03daefbcaa675ea7c3cc6997
Jon Gaspar <jongaspar@gmail.com>
2018-10-30 23:35:55 -0700

    planet images on travel page

--- client/src/components/screens/SpaceshipDealer.js
+++ client/src/components/screens/SpaceshipDealer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import { connect } from 'react-redux'
 import injectSheet from 'react-jss'
 import Button from 'components/reusables/Button'
-import getWeb3 from 'utils/getWeb3'
+import spaceship from 'assets/spaceship.jpg'
 
 const styles = {
   container: {
@@ -11,15 +11,16 @@ const styles = {
 }
 
 class SpaceshipDealer extends Component {
-  state = {};
+  state = {
+    isLoading: false,
+  };
 
   buySpaceship = async () => {
+    this.setState({ isLoading: true })
+
     const { web3 } = this.props
     const { gta } = this.props.contracts
     const { address } = this.props.player
-    console.log('gta', gta);
-
-    console.log('address', address);
 
     try {
       await gta.buySpaceship("My Spaceship", { from: address, value: web3.utils.toWei("0.01", "ether") })
@@ -27,6 +28,7 @@ class SpaceshipDealer extends Component {
       return console.error(e)
     }
 
+    this.props.goToTravel()
     // After success of spaceship buying, go to the travel screen
   }
 
@@ -37,11 +39,12 @@ class SpaceshipDealer extends Component {
       <div className={classes.container}>
         <h2>It's dangerous to go alone.</h2>
         <h1>Would you like to purchase a spaceship?</h1>
-        <h3>(spaceship ownership is required by law to handle commodities in this galactic region)</h3>
+        <h3>WARNING: Spaceship ownership is required by law to handle commodities in this galactic region</h3>
+        <img src={spaceship} />
         <Button
           onClick={this.buySpaceship}
           type="good"
-        >Okay</Button>
+        >{this.state.isLoading ? 'Loading...' : 'Okay'}</Button>
       </div>
     );
   }
--- client/src/components/screens/Travel.js
+++ client/src/components/screens/Travel.js
@@ -1,10 +1,33 @@
-import React, { Component } from "react";
-import { connect } from 'react-redux';
+import React, { Component } from "react"
+import { connect } from 'react-redux'
 import injectSheet from 'react-jss'
+
+import imgPlanet0 from 'assets/planet0.png'
+import imgPlanet1 from 'assets/planet1.png'
+import imgPlanet2 from 'assets/planet2.png'
+import imgPlanet3 from 'assets/planet3.png'
+import imgPlanet4 from 'assets/planet4.png'
+import imgPlanet5 from 'assets/planet5.png'
+import imgPlanet6 from 'assets/planet6.png'
+
+const planets = [
+  { name: "Mondopia", img: imgPlanet0, x: 5, y: 5 },
+  { name: "Zyrgon",   img: imgPlanet1, x: 2.25, y: 2.5 },
+  { name: "Ribos",    img: imgPlanet2, x: 0.25, y: 0.5 },
+  { name: "Mustafar", img: imgPlanet3, x: 4, y: 1 },
+  { name: "Arrakis",  img: imgPlanet4, x: 6, y: 3 },
+  { name: "Kronos",   img: imgPlanet5, x: 0.5, y: 4 },
+  { name: "4546B",    img: imgPlanet6, x: 2, y: 5.5 },
+]
 
 const styles = {
   container: {
-    textAlign: "center",
+  },
+  planets: {
+    width: '100%',
+  },
+  planet: {
+    position: 'absolute',
   }
 }
 
@@ -15,9 +38,25 @@ class Travel extends Component {
   render() {
     const { classes } = this.props
 
+    console.log('window.innerWidth', window.innerWidth);
+
     return (
       <div className={classes.container}>
         <h1>Travel</h1>
+        <div className={classes.planets}>
+          {planets.map((planet, i) =>
+            <img
+              key={i}
+              className={classes.planet}
+              src={planet.img}
+              width="128"
+              style={{
+                left: (window.innerWidth / 7) * planet.x,
+                bottom: (window.innerHeight / 7) * planet.y,
+              }}
+            />
+          )}
+        </div>
       </div>
     );
   }
