855658fd9f56d0eb893ed55571ce76e664b5b406
Jon Gaspar <jongaspar@gmail.com>
2018-11-03 10:32:49 -0700

    changed web3 version for industrialService to 0.20.1 (was 1.0.0-beta.36 before, but wasn't working as expected)

--- industrialService/server.js
+++ industrialService/server.js
@@ -1,122 +1,74 @@
 const Web3 = require("web3")
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 const truffleContract = require("truffle-contract")
-const gtaJSON = require("../build/contracts/GalacticTransitAuthority.json")
-const geaJSON = require("../build/contracts/GalacticEconomicAuthority.json")
 const giaJSON = require("../build/contracts/GalacticIndustrialAuthority.json")
 
 let investments = []
+let gia
 
-async function init() {
-  web3 = new Web3(new Web3.providers.WebsocketProvider("ws://localhost:8545"))
+function getContract(json) {
+  // Helper for initializing contracts
+  const contract = truffleContract(json)
+  contract.setProvider(web3.currentProvider)
+  return contract.deployed()
+}
 
-  let contractsArray = [
-    { json: gtaJSON, name: 'gta' },
-    { json: geaJSON, name: 'gea' },
-    { json: giaJSON, name: 'gia' },
-  ]
+function initGIA() {
+  return new Promise(async (resolve, reject) => {
+    gia = await getContract(giaJSON, web3)
+    const event = gia.InvestmentMade()
+    event.watch(async (err, res) => {
+      if (err) {
+        console.log('watch error', err)
+      }
+      else {
+        console.log('got an event', res)
+      }
+    })
 
-  const accounts = await web3.eth.getAccounts()
-  const owner = accounts[0]
-  console.log('owner', owner);
+    resolve()
+  })
+}
 
-  try {
-    contractsArray = await Promise.all(
-      contractsArray.map(_contract => new Promise(async (resolve, reject) => {
-        const contract = truffleContract(_contract.json)
-        contract.setProvider(web3.currentProvider)
+async function main() {
+  await initGIA()
 
-        // The next if block is from here:
-        // https://github.com/trufflesuite/truffle-contract/issues/57#issuecomment-331300494
-        if (typeof contract.currentProvider.sendAsync !== "function") {
-          contract.currentProvider.sendAsync = function() {
-            return contract.currentProvider.send.apply(
-              contract.currentProvider, arguments
-            );
-          };
-        }
 
-        try {
-          const instance = await contract.deployed()
-          resolve({ instance: instance, name: _contract.name })
-        } catch (e) {
-          reject(e)
-        }
-      }))
-    )
-  } catch (e) {
-    console.error(e)
-  }
 
-  // Store the contract instances in an object for easy reference
-  let contracts = {}
-  contractsArray.forEach(contract => contracts[contract.name] = contract.instance)
 
-  let blockNumber
-  try {
-    blockNumber = await web3.eth.getBlockNumber()
-  } catch (e) {
-    console.error(e)
-  }
+  /* Commented out this logic until I get the event listening working: */
 
-  const x = await contracts.gia.getCommodity(0)
-  console.log('x', x);
-  
+  // let blockNumber
+  // try {
+  //   blockNumber = await web3.eth.getBlockNumber()
+  // } catch (e) {
+  //   console.error(e)
+  // }
+
   //   // Adds investment info to investments array so we can loop over it
   //   investments.push({
   //     address: result.args.addr,
   //     blocksLeft: result.args.blocksLeft,
   //   })
 
-  // a list for saving subscribed event instances
-  const subscribedEvents = {}
-  // Subscriber method
-  const subscribeLogEvent = (contract, eventName) => {
-    const eventJsonInterface = web3.utils._.find(
-      contract._jsonInterface,
-      o => o.name === eventName && o.type === 'event',
-    )
-    const subscription = web3.eth.subscribe('logs', {
-      address: contract.address,
-      topics: [eventJsonInterface.signature]
-    }, (error, result) => {
-      if (!error) {
-        const eventObj = web3.eth.abi.decodeLog(
-          eventJsonInterface.inputs,
-          result.data,
-          result.topics.slice(1)
-        )
-        console.log(`New ${eventName}!`, eventObj)
-      }
-    })
-    subscribedEvents[eventName] = subscription
-  }
+  // web3.eth.subscribe('newBlockHeaders', async (a, b) => {
+  //   // console.log("New block headers seen:", a, b)
 
-  subscribeLogEvent(contracts.gia, "InvestmentMade")
-  
+  //   // Send mint commodity tx for every (active) investment once a new block is mined
+  //   investments.forEach(async (investment) => {
+  //     try {
+  //       await gia.mintCommodityFor(investment.address, { from: owner })
+  //       console.log("Minted for", investment.address)
+  //     } catch (e) {
+  //       return console.error("Could not mint for", investment.address, e)
+  //     }
 
-  console.log("Listening for InvestmentMade event...")
+  //     investment.blocksLeft--
+  //   })
 
-
-  web3.eth.subscribe('newBlockHeaders', async (a, b) => {
-    // console.log("New block headers seen:", a, b)
-
-    // Send mint commodity tx for every (active) investment once a new block is mined
-    investments.forEach(async (investment) => {
-      try {
-        await contracts.gia.mintCommodityFor(investment.address, { from: owner })
-        console.log("Minted for", investment.address)
-      } catch (e) {
-        return console.error("Could not mint for", investment.address, e)
-      }
-
-      investment.blocksLeft--
-    })
-
-    // Remove investments that have blocksLeft === 0
-    investments = investments.filter(investment => investment.blocksLeft > 0)
-  });
-
-  web3.eth.subscribe('logs', {}, console.log);
+  //   // Remove investments that have blocksLeft === 0
+  //   investments = investments.filter(investment => investment.blocksLeft > 0)
+  // });
 }
 
-init()
+main()
