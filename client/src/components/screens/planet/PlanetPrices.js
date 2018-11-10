285734a5ea2c64c893ec00335147b9edb254ffd4
Jon Gaspar <jongaspar@gmail.com>
2018-11-10 11:15:44 -0800

    commit before rearranging planetPrices to have commodity ids in parent array

--- client/src/components/screens/planet/PlanetPrices.js
+++ client/src/components/screens/planet/PlanetPrices.js
@@ -4,15 +4,19 @@ import injectSheet from 'react-jss'
 import planets from 'utils/planets'
 import MPIContainer from 'components/screens/planet/MPIContainer'
 import PricesRow from 'components/reusables/PricesRow'
+import uuid from 'utils/uuid'
 
 const styles = {}
 
 class PlanetPrices extends Component {
-  state = {};
+  state = {
+    commodityPrices: [],
+  }
 
   componentDidMount = async () => {
     const commodityInfos = await this.getCommodityInfos()
     console.log('commodityInfos', commodityInfos);
+    this.getAllSellOrders()
   }
 
   getCommodityInfos = async () => {
@@ -32,17 +36,73 @@ class PlanetPrices extends Component {
     })
   }
 
+  getAllSellOrders = async () => {
+    const { contracts, user, web3 } = this.props
+
+    // Collect all sell orders for each commodity on each planet
+    let planetPrices = []
+    for (let planetId = 0; planetId < 7; planetId++) {
+      const commodities = []
+      for (let commodityId = 0; commodityId < 7; commodityId++) {
+        const numSellOrders = await contracts.gea.getNumSellOrders(planetId, commodityId, { from: user.address })
+        const sellOrderIds = Array.apply(null, {length: parseInt(numSellOrders.toString())}).map(Number.call, Number)
+        const sellOrders = await Promise.all(sellOrderIds.map(sellOrderId => new Promise(async (resolve, reject) => {
+          const sellOrder = await contracts.gea.getSellOrder(planetId, commodityId, sellOrderId, { from: user.address })
+          resolve(sellOrder)
+        })))
+        commodities[commodityId] = sellOrders
+      }
+      planetPrices[planetId] = commodities
+    }
+
+    // Change array to only having min/max prices
+    planetPrices = planetPrices.map(planet => {
+      return planet.map(commodity => {
+        if (commodity.length) {
+          let min = commodity[0].price
+          let max = commodity[0].price
+
+          for (let i = 1; i < commodity.length; i++) {
+            if (commodity[i].price.lt(min)) {
+              min = commodity[i].price
+            }
+            if (commodity[i].price.gt(max)) {
+              max = commodity[i].price
+            }
+          }
+
+          return { min, max }
+        } else {
+          return { min: '-', max: '-' }
+        }
+      })
+    })
+
+    // Put the commodities in the parent array
+    // const commodityPrices = planetPrices.map(planet => {
+    //   let
+    //   return
+    // })
+
+    console.log('planetPrices', planetPrices);
+    this.setState({ planetPrices })
+  }
+
   render() {
     const { classes, user } = this.props
-    const planet = planets[user.currentPlanet]
+    const { planetPrices } = this.state
 
     return (
       <MPIContainer>
-        PlanetPrices
-        <PricesRow
-          pricesArray={[1, 2, 3]}
-          symbol={'BTC'}
-        />
+        {/* {planetPrices.map(planet => {
+          return (
+            <PricesRow
+              key={uuid()}
+              minMax={[1, 2, 3]}
+              symbol={'BTC'}
+            />
+          )
+        })} */}
       </MPIContainer>
     )
   }
