ed0560919d720879599e679b6ec2d96ca87326d1
Jon Gaspar <jongaspar@gmail.com>
2018-11-15 22:21:21 -0800

    renders 1 crystal with threejs

--- client/src/components/screens/planet/ViewCrystals.js
+++ client/src/components/screens/planet/ViewCrystals.js
@@ -9,6 +9,8 @@ import Dialog from 'components/reusables/Dialog'
 import SellOrder from 'components/reusables/SellOrder'
 import getPlayerInfo from 'utils/getPlayerInfo'
 import Loader from 'components/reusables/Loader'
+import * as THREE from 'three'
+
 
 const styles = {
   container: {
@@ -36,6 +38,38 @@ class ViewCrystals extends Component {
     this.crystalsOfOwner()
   }
 
+  componentDidUpdate = (_, prevState) => {
+    if (prevState.crystals.length !== this.state.crystals.length) {
+      this.renderCrystal()
+    }
+  }
+
+  renderCrystal = () => {
+    var scene = new THREE.Scene();
+    var cam = new THREE.PerspectiveCamera(100, window.innerWidth/window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+
+    const div = document.getElementById(1)
+    renderer.setSize(150, 100);
+    div.appendChild( renderer.domElement );
+
+    var box = new THREE.SphereGeometry(2, 10, -30);
+    var mesh = new THREE.MeshBasicMaterial({
+    wireframe: false,
+      color: 0x562399
+    });
+    var cube = new THREE.Mesh(box, mesh);
+    scene.add(cube);
+    cam.position.z = 3;
+    var render = function () {
+      requestAnimationFrame( render );
+      cube.rotation.y += 0.01;
+      renderer.render(scene, cam);
+    };
+
+    render();
+  }
+
   crystalsOfOwner = async () => {
     const { contracts, user } = this.props
     const crystals = []
@@ -54,7 +88,6 @@ class ViewCrystals extends Component {
       console.error(e)
     }
 
-    console.log('crystals', crystals);
     this.setState({ crystals })
   }
 
@@ -66,7 +99,7 @@ class ViewCrystals extends Component {
       <MPIContainer>
         <div className={classes.container}>
           {crystals.map((crystal, i) =>
-            <div key={i}>
+            <div key={i} id="1">
               {crystal.id}-{crystal.uri}
             </div>
           )}
