c7558fe21cade1c0920cf3f4bcedf33249b21308
Jon Gaspar <jongaspar@gmail.com>
2018-11-15 21:05:06 -0800

    comments and tidying

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,7 +16,6 @@ module.exports = function(deployer) {
     // There are separate contracts for each commodity because truffle doesn't seem to work
     // with multiple copies of the same contract at the current date
     // https://github.com/trufflesuite/truffle/issues/237
-
     const commodityArtifacts = [
       Commodity0,
       Commodity1,
@@ -49,16 +48,15 @@ module.exports = function(deployer) {
 
     // Deploy GTA
     await deployer.deploy(GalacticTransitAuthority)
-    // Store GTA instance
     const gta = await GalacticTransitAuthority.deployed()
 
     // Deploy GEA and GIA
     await deployer.deploy(GalacticEconomicAuthority, commodityAddresses, gta.address, { gas: 6000000 } )
     await deployer.deploy(GalacticIndustrialAuthority, commodityAddresses, gta.address, { gas: 6000000 })
-
-    // Set GEA and GIA for permissions
     const gea = await GalacticEconomicAuthority.deployed()
     const gia = await GalacticIndustrialAuthority.deployed()
+
+    // Set GEA and GIA access control
     await gta.setGEA(gea.address)
     await gta.setGIA(gia.address)
     await Promise.all(commodityInstances.map(instance => instance.setGEA(gea.address)))
@@ -67,10 +65,13 @@ module.exports = function(deployer) {
     // Deploy B. Crystal
     await deployer.deploy(ByzantianCrystal, { gas: 6000000 })
     const bCrystal = await ByzantianCrystal.deployed()
+
     // Deploy TA
     await deployer.deploy(TempleAuthority, commodityAddresses, gta.address, bCrystal.address, { gas: 6000000 })
     const temple = await TempleAuthority.deployed()
+
+    // Set TA access control
     await bCrystal.setTA(temple.address)
     return Promise.all(commodityInstances.map(instance => instance.setTA(temple.address)))
   })
-};
+}
