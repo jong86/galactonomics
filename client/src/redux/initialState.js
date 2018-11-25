a6faa55ccedc0033396cadb82a949215cf7e740e
Jon Gaspar <jongaspar@gmail.com>
2018-11-25 10:10:56 -0800

    fixed more merge conflicts

--- client/src/redux/initialState.js
+++ client/src/redux/initialState.js
@@ -3,10 +3,6 @@ export default {
     currentScreen: 'Welcome',
   },
   industrial: {
-<<<<<<< HEAD
     areasMined: [],
-=======
-    nonce: 0,
->>>>>>> dcad89e8ac1e39da820e27829a687e98ea4c21e8
   }
 }
