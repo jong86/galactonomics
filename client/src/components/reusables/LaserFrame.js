87f2b84e511d8e916bd2ee549fdbdb0b21e16f8e
Jon Gaspar <jongaspar@gmail.com>
2018-11-26 18:13:28 -0800

    changed capitalization of LaserFrame

---
+++ client/src/components/reusables/LaserFrame.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { connect } from 'react-redux'
+import injectSheet from 'react-jss'
+import colorFromType from 'utils/colorFromType'
+
+const styles = {
+  LaserFrame: {
+    textAlign: "center",
+    border: ({ type }) => '1px solid ' + colorFromType(type),
+    color: ({ type, active }) => {
+      // if (active) return '#000'
+      return colorFromType(type)
+    },
+    backgroundColor: ({ type, active }) => {
+      if (!active) return 'rgba(0, 0, 0, 0.75)'
+      return '#444'
+    },
+    width: ({ size }) => {
+      switch (size) {
+        case 'wide': return 'fill-available'
+        case 'small': return 'fit-content'
+        case 'wide3': return '33.33333%'
+        case 'wide4': return '25%'
+      }
+    },
+    borderRadius: 4,
+    padding: '12px',
+    margin: '4px',
+    cursor: ({ isButton }) => isButton ? 'pointer' : 'default',
+    userSelect: 'none',
+
+    '&:hover': {
+      color: ({ isButton }) => isButton ? '#000' : null,
+      backgroundColor: ({ isButton, type }) => isButton ? colorFromType(type) : null,
+    }
+  }
+}
+
+class LaserFrame extends React.Component {
+  render() {
+    const { classes, children, onClick } = this.props
+
+    return (
+      <div
+        className={classes.LaserFrame}
+        onClick={() => {
+          if (onClick) onClick()
+        }}
+      >
+        { children }
+      </div>
+    )
+  }
+}
+
+LaserFrame = injectSheet(styles)(LaserFrame)
+export default LaserFrame;
