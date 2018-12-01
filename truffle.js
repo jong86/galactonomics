f0bd517de63d79d4a1a957fb1c3af4a8482325e9
Jon Gaspar <jongaspar@gmail.com>
2018-11-30 20:06:21 -0800

    renamed redeploy to deploy

--- truffle.js
+++ truffle.js
@@ -5,5 +5,9 @@ module.exports = {
       port: 8545,
       network_id: "5777",
     },
+    ropsten: {
+      host: "https://ropsten.infura.io/v3/e3023fedad31499e899cec7b841ea70b",
+      network_id: "3",
+    },
   },
 };
