f41d05a6b08d30ef4996dd42af6ff1396d07fd22
Jon Gaspar <jongaspar@gmail.com>
2018-12-01 12:57:45 -0800

    added client/build folder

---
+++ client/build/precache-manifest.21e6b63976716631eb57f245aa995c4b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa6a9ca345cce3df9cfbac92facdabaf",
+    "url": "/static/media/temple.fa6a9ca3.png"
+  },
+  {
+    "revision": "97d73503cc016699fcca",
+    "url": "/static/js/main.97d73503.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9cbc836b72c4486052d2fcdae23bd485",
+    "url": "/static/media/spaceship.9cbc836b.jpg"
+  },
+  {
+    "revision": "d8e37266cc95f1bad299eb7ecb04ff5f",
+    "url": "/static/media/receivedSomething.d8e37266.wav"
+  },
+  {
+    "revision": "cd2085d30dfdcd5084952f3b8895990b",
+    "url": "/static/media/planet0.cd2085d3.png"
+  },
+  {
+    "revision": "06ea6d4b99eb35ac0a1204702309ca40",
+    "url": "/static/media/planet1.06ea6d4b.png"
+  },
+  {
+    "revision": "d673fc3fc1c33e7063173668a4a9cb92",
+    "url": "/static/media/planet2.d673fc3f.png"
+  },
+  {
+    "revision": "7994fc8c8389af37ff4eef2b586fee1d",
+    "url": "/static/media/planet3.7994fc8c.png"
+  },
+  {
+    "revision": "e55e01aae9f2b14e19104196c7023447",
+    "url": "/static/media/planet4.e55e01aa.png"
+  },
+  {
+    "revision": "0d7f8301f204901dfff0874483fec55f",
+    "url": "/static/media/planet5.0d7f8301.png"
+  },
+  {
+    "revision": "bb4a79113e1c620af4d218b2d62ecf33",
+    "url": "/static/media/planet6.bb4a7911.png"
+  },
+  {
+    "revision": "f4b5ea1496221d0a1d289bc863512d27",
+    "url": "/static/media/aCrystalWasForged.f4b5ea14.wav"
+  },
+  {
+    "revision": "047cef6619f480282d132cd96d4b9aa0",
+    "url": "/static/media/planet0.047cef66.wav"
+  },
+  {
+    "revision": "fa9efbdc007ca137c015e5981694ab69",
+    "url": "/static/media/planet1.fa9efbdc.wav"
+  },
+  {
+    "revision": "bb93aba8778841b9a220cdec0e1e477d",
+    "url": "/static/media/planet2.bb93aba8.wav"
+  },
+  {
+    "revision": "4dbe1f813014ece09bfa9f6c921a4f68",
+    "url": "/static/media/planet3.4dbe1f81.wav"
+  },
+  {
+    "revision": "a16852173697d5d555678a1f132ad8c9",
+    "url": "/static/media/planet4.a1685217.wav"
+  },
+  {
+    "revision": "59338ea4292e5570a0a7af70597f9444",
+    "url": "/static/media/planet5.59338ea4.wav"
+  },
+  {
+    "revision": "ebc4639abb07c898c5f5ff29ba0605ca",
+    "url": "/static/media/planet6.ebc4639a.wav"
+  },
+  {
+    "revision": "b3d5d527cd2717904c285ea4ed793426",
+    "url": "/static/media/telegrama.b3d5d527.woff"
+  },
+  {
+    "revision": "5ff78101204e533ca8b6f9918435b53b",
+    "url": "/static/media/travelling.5ff78101.wav"
+  },
+  {
+    "revision": "10ca65022458dc0da4cfb5229b23bdae",
+    "url": "/static/media/miningSuccess.10ca6502.wav"
+  },
+  {
+    "revision": "0a6fc2bf0a733ca43eee",
+    "url": "/static/js/1.0a6fc2bf.chunk.js"
+  },
+  {
+    "revision": "5da21cd110186e49ddbf4601ab4f19be",
+    "url": "/index.html"
+  }
+];
