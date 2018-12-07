8d8ea378cb139435e0b2ecb59ba31cbb97e88f70
Jon Gaspar <jongaspar@gmail.com>
2018-12-07 13:24:04 -0800

    moved GIA stuff to Commodities. deleted GIA

--- contracts/Commodities.sol
+++ contracts/Commodities.sol
@@ -2,74 +2,134 @@ pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "./items/Commodity.sol";
-import "./interfaces/ICommodity.sol";
-import "./interfaces/ICommodities.sol";
+import "./libraries/AddressCast.sol";
+import "./libraries/BytesCast.sol";
+import "./libraries/UintCast.sol";
 
 /**
  * @title Commodities
  *
  * @notice This contracts is for convenient access to the commodities
  */
-contract Commodities is ICommodities, Ownable {
+contract Commodities is Ownable {
   using SafeMath for uint;
+  using AddressCast for address;
+  using BytesCast for bytes32;
+  using UintCast for uint8;
+  using UintCast for uint;
 
-  // Array storing all info for each commodity
-  ICommodity[7] public commodities;
+  IGalacticTransitAuthority gta;
 
-  constructor(address[] _commodities) public {
-    for (uint i = 0; i < _commodities.length; i++) {
-      commodities[i] = ICommodity(_commodities[i]);
-    }
+  // Mapping of commodityId to address to amount of commodity owned
+  mapping (uint => mapping (address => uint)) balances;
+
+  // Mapping of address to array containing Ids of commodities owned (for iterating)
+  mapping (address => uint[]) commoditiesOwned;
+
+  // Mapping of commodityId to previous mining hash
+  mapping (uint => bytes32) prevMiningHashes;
+
+  constructor(address _gta) public {
+    gta = IGalacticTransitAuthority(_gta);
   }
 
+  event CommodityMined(bytes32 _hash, address _miner);
+
+  /**
+   * @notice Mints new commodity tokens for a player
+   * @param _nonce Value found that when hashed (using SHA-256) with the previous proof-of-work hash found for a
+   *  commodity, results in an acceptable hash according to current target for that commodity
+   */
+  function mine(string _nonce) external {
+    require(gta.isPlayer(msg.sender), "You must own a spaceship for this action");
+    uint8 _commodityId = gta.getCurrentPlanet(msg.sender);
+    require(
+      gta.canFitCargo(
+        msg.sender,
+        getCurrentCargo(msg.sender),
+        getMiningReward(_commodityId)
+      ),
+      "Not enough cargo capacity available on your ship, you must unload cargo by selling it"
+    );
+
+    string memory _prevHash = prevMiningHashes[_commodityId].toString();
+
+    bytes32 _hash = sha256(
+      abi.encodePacked(
+        _nonce,
+        _commodityId.toString(),
+        _prevHash,
+        msg.sender.toString()
+      )
+    );
+
+    require(mint(msg.sender, getMiningReward(_commodityId)), "Error sending reward");
+    emit CommodityMined(_hash, msg.sender);
+  }
+
+  /**
+   * @notice Returns total cargo stored on player's ship
+   * @param _player Address of player to look up
+   */
   function getCurrentCargo(address _player) external view returns (uint) {
     uint currentCargo;
-    for (uint8 i = 0; i < commodities.length; i++) {
-      uint cargoToAdd = commodities[i].balanceOf(_player);
+    for (uint i = 0; i < commoditiesOwned[_player].length; i++) {
+      uint cargoToAdd = commoditiesOwned[_player][i];
       currentCargo = currentCargo.add(cargoToAdd);
     }
     return currentCargo;
   }
 
-  function get(uint8 _id) external view returns (
+  /**
+   * @notice Returns data on a commodity
+   * @param _id Id of commodity
+   */
+  function getCommodity(uint _id) external view returns (
     string name,
-    string symbol,
+    uint balance,
     uint miningReward,
     bytes32 miningTarget,
     bytes32 prevMiningHash
   ) {
     return (
-      commodities[_id].name(),
-      commodities[_id].symbol(),
-      commodities[_id].miningReward(),
-      commodities[_id].miningTarget(),
-      commodities[_id].prevMiningHash()
+      getName(_id),
+      balances[_id][msg.sender],
+      getMiningReward(_id),
+      getMiningTarget(_id),
+      prevMiningHashes[_id]
     );
   }
 
-  function getName(uint8 _id) external view returns (string) {
-    return commodities[_id].name();
+  /**
+   * @notice Returns URI of a commodity
+   * @param _id Id of commodity
+   */
+  function getURI(uint _id) public view returns (string) {
+    return sha256(abi.encodePacked(_id.toString()));
   }
 
-  function getSymbol(uint8 _id) external view returns (string) {
-    return commodities[_id].symbol();
+  /**
+   * @notice Returns mining reward of a commodity
+   * @param _id Id of commodity
+   */
+  function getMiningReward(uint _id) public view returns (uint) {
+    return 10;
   }
 
-  function getMiningReward(uint8 _id) external view returns (uint) {
-    return commodities[_id].miningReward();
+  /**
+   * @notice Returns mining target of a commodity
+   * @param _id Id of commodity
+   */
+  function getMiningTarget(uint _id) public view returns (bytes32) {
+    return sha256("0");
   }
 
-  function getMiningTarget(uint8 _id) external view returns (bytes32) {
-    return commodities[_id].miningTarget();
-  }
-
-  function getBalance(uint8 _id) external view returns (uint) {
-    return commodities[_id].balanceOf(msg.sender);
-  }
-
-  function getInterface(uint8 _id) external view returns (ICommodity) {
-    return commodities[_id];
+  /**
+   * @notice Returns amount of commodity the player owns
+   * @param _id Id of commodity
+   */
+  function getBalance(uint _id) public view returns (uint) {
+    return balances[_id][msg.sender];
   }
 
   function() public {}
---
+++ test/commodities.js
@@ -0,0 +1,101 @@
+const Commodities = artifacts.require("./Commodities.sol")
+const GalacticTransitAuthority = artifacts.require("./GalacticTransitAuthority.sol")
+const GalacticEconomicAuthority = artifacts.require("./GalacticEconomicAuthority.sol")
+const GalacticIndustrialAuthority = artifacts.require("./GalacticIndustrialAuthority.sol")
+const { fillUpCargoByMining, mineCommodityXTimes } = require('./util/testUtils')
+const sha256 = require('js-sha256');
+
+contract("GalacticIndustrialAuthority", accounts => {
+  let gta, gea, gia, commodities
+  const player1 = accounts[1]
+  const player2 = accounts[2]
+
+  beforeEach(async() => {
+    // Deploy main contracts
+    commodities = await Commodities.new()
+    gta = await GalacticTransitAuthority.new()
+    gea = await GalacticEconomicAuthority.new(gta.address)
+    gia = await GalacticIndustrialAuthority.new(gta.address)
+    // Set access roles
+    await gta.setGEA(gea.address)
+    await gta.setGIA(gia.address)
+
+    const costOfSpaceship = await gta.costOfSpaceship()
+    await gta.buySpaceship("A", { from: player1, value: costOfSpaceship })
+    await gta.buySpaceship("B", { from: player2, value: costOfSpaceship })
+    await gta.travelToPlanet(0, { from: player1 })
+    await gta.travelToPlanet(0, { from: player2 })
+  })
+
+  it("mints commodity for player when player submits valid proof-of-work", async () => {
+    const miningData = await gia.getMiningData({ from: player1 })
+    const miningReward = miningData[0]
+    const miningTarget = miningData[1]
+    const prevHash = miningData[2]
+
+    let nonce = 3500
+    let hash
+    do {
+      nonce++
+      hash = sha256(
+        nonce.toString() +
+        (0).toString() +
+        prevHash +
+        player1.substring(2)
+      )
+    } while (parseInt(hash, 16) >= parseInt(miningTarget, 16))
+
+    console.log('prevHash', prevHash);
+    console.log('hash', hash);
+
+    const response = await gia.submitProofOfWork(String(nonce), { from: player1 })
+    const hashFromSolidity = response.logs.find(log => log.event === 'CommodityMined').args._hash
+
+    const balance = await commodities.getBalance(0, { from: player1 })
+
+    assert.equal(balance.toString(), miningReward.toString(), "did not receive mining reward")
+    assert.equal('0x' + hash, hashFromSolidity, "hash from javascript does not match hash from solidity")
+  })
+
+  it("does not mint commodity if user submits invalid proof-of-work", async () => {
+    const balanceBefore = await commodities.getBalance(0, { from: player1 })
+    try {
+      await gia.submitProofOfWork(0, { from: player1 })
+    } catch (e) {}
+    const balanceAfter = await commodities.getBalance(0, { from: player1 })
+    assert.equal(balanceBefore.toString(), balanceAfter.toString(), "did not receive mining reward")
+  })
+
+  it("does not mint if player cannot fit the mining reward in spaceship's cargo", async () => {
+    await fillUpCargoByMining(commodities, gta, gia, player1, 0)
+    const balanceBefore = await commodities.getBalance(0, { from: player1 })
+
+    const miningData = await gia.getMiningData({ from: player1 })
+    const miningTarget = miningData[1]
+    const prevHash = miningData[2]
+
+    let nonce = 3500
+    let hash
+    do {
+      nonce++
+      hash = sha256(
+        nonce.toString() +
+        (0).toString() +
+        prevHash +
+        player1.substring(2)
+      )
+    } while (parseInt(hash, 16) >= parseInt(miningTarget, 16))
+
+    try {
+      await gia.submitProofOfWork(String(nonce), { from: player1 })
+    } catch (e) {}
+
+    const balanceAfter = await commodities.getBalance(0, { from: player1 })
+
+    assert.equal(
+      balanceBefore.toString(),
+      balanceAfter.toString(),
+      "balance should not have changed after calling submitProofOfWork"
+    )
+  })
+})
