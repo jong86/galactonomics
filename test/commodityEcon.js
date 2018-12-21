1c04ee0b586d357bf6073f3fde19b17e5eb433fe
Jon Gaspar <jongaspar@gmail.com>
2018-12-21 00:02:49 -0800

    delete unused tests

--- test/commodityEcon.js
+++ test/commodityEcon.js
@@ -1,11 +1,10 @@
 const CommodityReg = artifacts.require("./CommodityReg.sol")
 const CommodityEcon = artifacts.require("./CommodityEcon.sol")
-const { fillUpCargoByMining, mineCommodityXTimes, getRandomPlanetToSell } = require('./util/testUtils')
+const { mineCommodityXTimes } = require('./util/testUtils')
 const truffleAssert = require('truffle-assertions');
 
-
 contract("CommodityEcon", accounts => {
-  let transitAuthority, commodityEcon, gia, commodities, allCommodities, tradedOnPlanet
+  let commodityEcon, commodityReg
   const player1 = accounts[1]
   const player2 = accounts[2]
   const qty = 5
@@ -78,56 +77,5 @@ contract("CommodityEcon", accounts => {
 
     // Assert record of buyer address
     assert.equal(order[4], player2, "did not record player2 as buyer")
-  })
-
-  it("should not let non-player buy player1's sell order", async () => {
-    await mineCommodityXTimes(gia, 4, player1, 0)
-
-    const planetToSell = getRandomPlanetToSell(0, tradedOnPlanet)
-
-    await transitAuthority.travelToPlanet(planetToSell, { from: player1 })
-    await commodityEcon.createSellOrder(planetToSell, 0, qty, price, { from: player1 })
-    try {
-      await commodityEcon.buySellOrder(planetToSell, orderId, { from: nonPlayer, value: qty * price })
-    } catch (e) {
-      return assert(true)
-    }
-    assert(false, "could buy sell-order")
-  })
-
-  it("should fail if player cannot fit the cargo they want to buy", async () => {
-    // Fill up player2's cargo
-    try {
-      await fillUpCargoByMining(commodities, transitAuthority, gia, player2, 0)
-    } catch (e) {
-      return assert(false, e.toString())
-    }
-    // Find a amount of cargo that will max out player2's cargo
-    const currentCargo = await commodities.getCurrentCargo(player2)
-    const availableCargo = await transitAuthority.getAvailableCargo(player2, currentCargo)
-
-    // Create a sell order with player1 that is too much cargo for player2
-    const miningReward = await commodities.getMiningReward(0)
-    const timesToMint = availableCargo.div(miningReward)
-    try {
-      await mineCommodityXTimes(gia, timesToMint.add(1), player1, 0)
-    } catch (e) {
-      return assert(false, e.toString())
-    }
-
-    const planetToSell = getRandomPlanetToSell(0, tradedOnPlanet)
-
-    await transitAuthority.travelToPlanet(planetToSell, { from: player1 })
-    const response = await commodityEcon.createSellOrder(planetToSell, 0, availableCargo.add(1), price, { from: player1 })
-
-    const { orderId } = response.logs[0].args
-
-    try {
-      await commodityEcon.buySellOrder(planetToSell, orderId, { from: player2, value: qty * price })
-    } catch (e) {
-      return assert(true)
-    }
-
-    assert(false, "did not revert")
   })
 })
